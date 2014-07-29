859954678fc0e0bde591044137bf8fa9ea7956e8
mhearn-av <matt.hearn@availity.com>
2014-07-29 12:53:19 -0500

    Added TrackDataParser solution. Removed old files from credit_card_track_parser

---
+++ TrackDataParser/TrackDataParser/TrackDataParser.ts
@@ -0,0 +1,140 @@
+﻿
+interface ITrackData {
+    startSentinel: string;
+    primaryAccountNumber: string;
+    fieldSeparator: string;
+    expirationDate: string;
+    serviceCode: string;
+    discretionaryData: string;
+    endSentinel: string;
+}
+
+interface ITrack1FormatB extends ITrackData {
+    name: string;
+    formatCode: string;
+}
+
+class TrackData implements ITrackData {
+    startSentinel: string;  
+    primaryAccountNumber: string;
+    fieldSeparator: string;
+    expirationDate: string;
+    serviceCode: string;
+    discretionaryData: string;
+    endSentinel: string;
+    rawTrackData: string;
+    trackRegex: RegExp;
+}
+
+
+class Track1FormatB extends TrackData implements ITrack1FormatB{
+    // Example of Track1FormatB %B5266092201416174^FATEHI/SUALEH^16042010000000000000000000000000000567001000?
+    // http://en.wikipedia.org/wiki/Magnetic_stripe_card
+    trackRegex = new RegExp("(%)(B)([0-9]{1,19})(\\^)([^\\^]{2,26})\\^([0-9]{4}|\\^)([0-9]{3}|\\^)([^\\?]+)(\\?)");
+    formatCode: string;
+    name: string;
+
+    constructor(rawTrackData: string) {
+        super();
+        this.rawTrackData = rawTrackData;
+        if (this.hasValidTrack1FormatB()) {
+            this.setTrackData();
+        }
+        
+    }
+
+    hasValidTrack1FormatB(): boolean {
+        
+        return this.trackRegex.test(this.rawTrackData);
+
+    }
+
+    getParsedTrackDataArray(): RegExpExecArray {
+        return this.trackRegex.exec(this.rawTrackData);
+        
+    }
+
+    setTrackData() {
+        var trackData: RegExpExecArray = this.getParsedTrackDataArray();
+        trackData.shift();
+        this.startSentinel = trackData[0];
+        this.formatCode = trackData[1];
+        this.primaryAccountNumber = trackData[2];
+        this.fieldSeparator = trackData[3];
+        this.name = trackData[4];
+        this.expirationDate = trackData[5];
+        this.serviceCode = trackData[6];
+        this.discretionaryData = trackData[7];
+        this.endSentinel = trackData[8];
+
+    }
+}
+
+class Track2 extends TrackData{
+    // http://en.wikipedia.org/wiki/Magnetic_stripe_card
+    //Example of track 2 %B5266092201416174^FATEHI/SUALEH^16042010000000000000000000000000000567001000?\n;5266092201416174=16042010000056700100?
+    trackRegex = new RegExp("(;)([0-9]{1,19})(=)([0-9]{1,4}|=)([0-9]{1,3}|=)([0-9]*)(\\?)");
+
+    constructor(rawTrackData: string) {
+        super();
+        this.rawTrackData = rawTrackData;
+        if (this.hasValidTrack2()) {
+            this.setTrackData();
+        }
+
+    }
+
+    getParsedTrackDataArray() {
+        return this.trackRegex.exec(this.rawTrackData);  
+    }
+
+    setTrackData() {
+        var trackData: RegExpExecArray = this.getParsedTrackDataArray();
+        trackData.shift();
+        this.startSentinel = trackData[0];
+        this.primaryAccountNumber = trackData[1];
+        this.fieldSeparator = trackData[2];
+        this.expirationDate = trackData[3];
+        this.serviceCode = trackData[4];
+        this.discretionaryData = trackData[5];
+        this.endSentinel = trackData[6];
+
+    }
+
+    hasValidTrack2(): boolean {
+        return this.trackRegex.test(this.rawTrackData);
+    }
+}
+
+class ParsedTrackData {
+    HasValidTrack1: boolean;
+    HasValidTrack2: boolean;
+    ParsedTrack1FormatB: ITrack1FormatB;
+    ParsedTrack2: ITrackData;
+
+    constructor(parsedTrack1: Track1FormatB, parsedTrack2: Track2) {
+        this.ParsedTrack2 = parsedTrack2;
+        this.ParsedTrack1FormatB = parsedTrack1;
+    }    
+}
+
+class TrackDataParser
+{
+    hasValidTrack1: boolean;
+    hasValidTrack2: boolean;
+    ParsedTrack1FormatB: Track1FormatB;
+    ParsedTrack2: Track2;
+    ParsedTrackData: ParsedTrackData;
+
+    Parse(rawTrackData: string): ParsedTrackData {
+        this.ParsedTrack2 = new Track2(rawTrackData);
+        this.hasValidTrack2 = this.ParsedTrack2.hasValidTrack2();
+        this.ParsedTrack1FormatB = new Track1FormatB(rawTrackData);
+        this.hasValidTrack1 = this.ParsedTrack1FormatB.hasValidTrack1FormatB();
+        this.ParsedTrackData = new ParsedTrackData(this.ParsedTrack1FormatB, this.ParsedTrack2);
+        this.ParsedTrackData.HasValidTrack1 = this.hasValidTrack1;
+        this.ParsedTrackData.HasValidTrack2 = this.hasValidTrack2;
+
+        return this.ParsedTrackData;
+    }
+}
---
+++ TrackDataParser/TrackDataParser/test/TrackDataParser.ts
@@ -0,0 +1,102 @@
+﻿///<reference path="../typings/qunit.d.ts"/>
+///<reference path="../TrackDataParser.ts"/>
+
+/*
+Tracks for testing
+%B5266092201416174^FATEHI/SUALEH^16042010000000000000000000000000000567001000?
+%B5266092201416174^FATEHI/SUALEH^16042010000000000000000000000000000567001000?\n;5266092201416174=16042010000056700100?
+%B5266092201416174^FATEHI/SUALEH^16042010000000000000000000000000000567001000?;5266092201416174=16042010000056700100?
+%B5266092201416174^FATEHI/SUALEH^16042010000000000000000000000000000567001000?;5266092201416174==2010000056700100?
+%B5266092201416174^FATEHI/SUALEH^16042010000000000000000000000000000567001000?;5266092201416174===0000056700100?
+*/
+
+
+test("hasValidTrack1 true", function() {
+    var trackData: Track1FormatB = new Track1FormatB("%B5266092201416174^FATEHI/SUALEH^16042010000000000000000000000000000567001000?");
+    equal(true, trackData.hasValidTrack1FormatB(), "hasValidTrack1FormatB: " + trackData.hasValidTrack1FormatB());
+});
+
+test("getParsedTrackDataArray Track1FormatB match", function() {
+    var trackData: Track1FormatB = new Track1FormatB("%B5266092201416174^FATEHI/SUALEH^16042010000000000000000000000000000567001000?");
+    var track1FormatBArray: RegExpExecArray = trackData.getParsedTrackDataArray();
+    ok(track1FormatBArray != null);
+});
+
+//how to handle formats aside from B?
+test("getParsedTrackDataArray Track1FormatB unsupported format", function () {
+    var trackData: Track1FormatB = new Track1FormatB("%C5266092201416174^FATEHI/SUALEH^16042010000000000000000000000000000567001000?");
+    var track1FormatBArray: RegExpExecArray = trackData.getParsedTrackDataArray();
+    ok(track1FormatBArray == null);
+});
+
+test("getParsedTrackDataArray Track1FormatB no match missing starting sentinel", function () {
+    var trackData: Track1FormatB = new Track1FormatB("B15266092201416174^FATEHI/SUALEH^16042010000000000000000000000000000567001000?");
+    var track1FormatBArray: RegExpExecArray = trackData.getParsedTrackDataArray();
+    ok(track1FormatBArray == null);
+});
+
+test("getParsedTrackDataArray Track2 match", function () {
+    var trackData: Track1FormatB = new Track1FormatB("%B5266092201416174^FATEHI/SUALEH^16042010000000000000000000000000000567001000?\n;5266092201416174=16042010000056700100?");
+    var track1FormatBArray: RegExpExecArray = trackData.getParsedTrackDataArray();
+    ok(track1FormatBArray != null);
+});
+
+test("getParsedTrackDataArray Track2 no match missing starting sentinel", function () {
+    var trackData: Track2 = new Track2("%B5266092201416174^FATEHI/SUALEH^16042010000000000000000000000000000567001000?\n5266092201416174=16042010000056700100?");
+    var track1FormatBArray: RegExpExecArray = trackData.getParsedTrackDataArray();
+    ok(track1FormatBArray == null);
+});
+
+test("hasValidTrack2 true", function () {
+    var trackData: Track2 = new Track2("%B5266092201416174^FATEHI/SUALEH^16042010000000000000000000000000000567001000?\n;5266092201416174=16042010000056700100?");
+    equal(true, trackData.hasValidTrack2(), "hasValidTrack2: " + trackData.hasValidTrack2());
+});
+
+test("setTrackData Track1FormatB valid no missing information", function() {
+    var trackData: Track1FormatB = new Track1FormatB("%B5266092201416174^FATEHI/SUALEH^16042010000000000000000000000000000567001000?\n;5266092201416174=16042010000056700100?");
+    ok(trackData.startSentinel == "%", "startSentinel");
+    ok(trackData.formatCode == "B", "formatCode");
+    ok(trackData.primaryAccountNumber == "5266092201416174", "primaryAccountNumber");
+    ok(trackData.fieldSeparator == "^", "fieldSeparator");
+    ok(trackData.name == "FATEHI/SUALEH", "name");
+    ok(trackData.expirationDate == "1604", "expiration date");
+    ok(trackData.serviceCode == "201", "service code");
+    ok(trackData.discretionaryData == "0000000000000000000000000000567001000", "discretionary data: " + trackData.discretionaryData);
+    ok(trackData.endSentinel == "?", "endSentinel: " + trackData.endSentinel);
+});
+
+test("setTrackData Track1FormatB valid missing information", function () {
+    var trackData: Track1FormatB = new Track1FormatB("%B5266092201416174^FATEHI/SUALEH^^^0000000000000000000000000000567001000?\n;5266092201416174=16042010000056700100?");
+    ok(trackData.startSentinel == "%", "startSentinel");
+    ok(trackData.formatCode == "B", "formatCode");
+    ok(trackData.primaryAccountNumber == "5266092201416174", "primaryAccountNumber");
+    ok(trackData.fieldSeparator == "^", "fieldSeparator");
+    ok(trackData.name == "FATEHI/SUALEH", "name");
+    ok(trackData.expirationDate == "^", "expiration date");
+    ok(trackData.serviceCode == "^", "service code");
+    ok(trackData.discretionaryData == "0000000000000000000000000000567001000", "discretionary data: " + trackData.discretionaryData);
+    ok(trackData.endSentinel == "?", "endSentinel: " + trackData.endSentinel);
+});
+
+test("setTrackData Track 2 valid no missing information", function () {
+    var trackData: Track2 = new Track2("%B5266092201416174^FATEHI/SUALEH^16042010000000000000000000000000000567001000?\n;5266092201416174=16042010000056700100?");
+    ok(trackData.startSentinel == ";");
+    ok(trackData.primaryAccountNumber == "5266092201416174");
+    ok(trackData.fieldSeparator == "=", "fieldSeparator");
+    ok(trackData.expirationDate == "1604");
+    ok(trackData.serviceCode == "201");
+    ok(trackData.discretionaryData == "0000056700100");
+    ok(trackData.endSentinel == "?");
+});
+
+test("setTrackData Track 2 valid missing information", function () {
+    var trackData: Track2 = new Track2("%B5266092201416174^FATEHI/SUALEH^16042010000000000000000000000000000567001000?\n;5266092201416174===0000056700100?");
+    ok(trackData.startSentinel == ";", "startSentinel");
+    ok(trackData.primaryAccountNumber == "5266092201416174", "primary account number");
+    ok(trackData.fieldSeparator == "=", "fieldSeparator");
+    ok(trackData.expirationDate == "=", "expiration date");
+    ok(trackData.serviceCode == "=", "service code");
+    ok(trackData.discretionaryData == "0000056700100", "discretionary data");
+    ok(trackData.endSentinel == "?", "endSentinel");
+});
+
